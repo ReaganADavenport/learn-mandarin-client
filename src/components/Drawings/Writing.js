f03a70233f6d48820c97b5e4de923d8f3222208b
Reagan Davenport <reagandavenport@Reagans-MacBook-Air.local>
2019-07-23 09:32:37 -0400

    figured out how to resize the CanvasDraw

--- src/components/Drawings/Writing.js
+++ src/components/Drawings/Writing.js
@@ -5,15 +5,21 @@ import CanvasDraw from "react-canvas-draw";
 class Writing extends Component {
     state = {
         color: "#ffc600",
-        width: 400,
-        height: 400,
-        brushRadius: 10,
-        lazyRadius: 12
+        width: 200,
+        height: 200,
+        brushRadius: 7,
+        lazyRadius: 1
     };
 
     render(){
         return(
-        <CanvasDraw/>
+            <CanvasDraw
+            ref={canvasDraw => (this.saveableCanvas = canvasDraw)}
+            brushRadius={this.state.brushRadius}
+            lazyRadius={this.state.lazyRadius}
+            canvasWidth={this.state.width}
+            canvasHeight={this.state.height}
+        />
         )
     }
 }
