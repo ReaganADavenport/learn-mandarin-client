ae0bf97542ab5f60df653f34f284d4c0b637f52c
Reagan Davenport <reagandavenport@Reagans-MacBook-Air.local>
2019-07-29 16:53:02 -0400

    finally getting the resemble to convert the answer keys into data strings

--- src/components/WriteTests/Resemble.js
+++ src/components/WriteTests/Resemble.js
@@ -14,45 +14,38 @@ class Resemble extends Component {
         // const _this = this;
         console.log("Image1 is", Image1);
         console.log("user guess image is", this.props.Guess);
-        // const url = '../../images/answerCards/Image1.png'
-        // console.log("src is ", url);
-        const dataUri = await this.generateImageFromAnswers(Image1);
-        console.log("dataUri is", dataUri);
-        this.compare(dataUri);
-        this.setState({
-            someImage: dataUri
-        })
-
+        
+        this.getDataUrl(Image1, dataUri =>{
+            this.compare(dataUri, this.props.Guess);
+            console.log("dataUri is", dataUri);
+        });
     };
 
-    generateImageFromAnswers = async (imageFile) => {
-        const image = new Image();//Okay so we are making a new "Image" and we are calling it image
-        image.src = imageFile;//the origin of the image is what I pass in
-        const imageString = this.getDataUrl(image);
-        console.log("image string", imageString);
-    }
 
-    getDataUrl = (img) => {
-        console.log("img is", img);
+    getDataUrl = (img, callback) => {
+        const image = new Image();//Okay so we are making a new "Image" and we are calling it image
+
+        image.onload = function() {
             const canvas = document.createElement('canvas');//makeing a new "canvas" and we are calling it canvas
             const ctx = canvas.getContext('2d');//working with a 2d image
 
-            canvas.width = img.width; //make the canvas the same width as image
-            canvas.height = img.height; //make the cavas the same height as image
-            console.log("canvas", img.height);
-            ctx.drawImage(img, 0, 0); //draw a 2d image useing image at position 0,0.
-            const imageString = canvas.toDataURL();
-            console.log("image string after data URL", imageString);
-            return imageString;
+            canvas.width = this.width; //make the canvas the same width as image
+            canvas.height = this.height; //make the cavas the same height as image
+            
+            ctx.drawImage(this, 0, 0); //draw a 2d image useing image at position 0,0.
+            
+            callback(canvas.toDataURL('image/png'));
+        };
+        image.src = img;
     };
 
     setTrueOrFalse = data => {
         // console.log("data is", data);
         let value = null
         if(data.rawMisMatchPercentage < 95){
-            value = false;
-        } else {
             value = true;
+        } else {
+            value = false;
         }
 
         this.setState({
